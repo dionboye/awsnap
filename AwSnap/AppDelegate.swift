fa3477256b0ef63615f8471edb039c50f54213c0
Pier Fumagalli <pier@usrz.com>
2016-05-17 17:26:28 +0900

    Cleanup

--- AwSnap/AppDelegate.swift
+++ AwSnap/AppDelegate.swift
@@ -5,54 +5,38 @@ import AVFoundation
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
-    @IBOutlet weak var view: NSView!
-    @IBOutlet weak var settings: NSWindow!
-    @IBOutlet weak var click: NSButton!
 
+    private var _device:AVCaptureDevice? = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo);
     private var _session:AVCaptureSession = AVCaptureSession()
     private var _capture:AVCaptureStillImageOutput = AVCaptureStillImageOutput()
+    private var _layer:AVCaptureVideoPreviewLayer = AVCaptureVideoPreviewLayer()
     private var _count:Int32 = 0
+
+    /* ====================================================================== */
+    /* NSApplicationDelegate                                                  */
+    /* ====================================================================== */
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
 
-        // Get our device
-        let device:AVCaptureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        if _device != nil {
 
-        // Position the window smack in the middle of the screen
-        let description = device.activeFormat.formatDescription
-        let dimensions:CMVideoDimensions = CMVideoFormatDescriptionGetDimensions(description)
+            // Position the window smack in the middle of the screen
+            let description = _device!.activeFormat.formatDescription
+            let dimensions:CMVideoDimensions = CMVideoFormatDescriptionGetDimensions(description)
 
-        let width = CGFloat(dimensions.width)
-        let height = CGFloat(dimensions.height)
+            let width = CGFloat(dimensions.width)
+            let height = CGFloat(dimensions.height)
 
-        let screen = window.screen!.frame
+            let screen = window.screen!.frame
 
-        let x = (screen.width - width) / 2
-        let y = (screen.height - height) / 2
+            let x = (screen.width - width) / 2
+            let y = (screen.height - height) / 2
 
-        let frame = NSRect(x: x, y: y, width: width, height: height)
-        window.setFrame(frame, display: true, animate: false)
+            let frame = NSRect(x: x, y: y, width: width, height: height)
+            window.setFrame(frame, display: true, animate: false)
 
-        // Start the preview
-        do {
-            let input:AVCaptureInput = try AVCaptureDeviceInput(device:device)
-            _session.addInput(input)
-
-            _capture.outputSettings = [ AVVideoCodecKey: AVVideoCodecJPEG ]
-            _session.addOutput(_capture)
-
-            let previewLayer:CALayer = view.layer!
-
-            let videoLayer = AVCaptureVideoPreviewLayer(session: _session)
-            videoLayer.frame = previewLayer.bounds
-            videoLayer.autoresizingMask = CAAutoresizingMask.LayerWidthSizable.union(CAAutoresizingMask.LayerHeightSizable)
-            videoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
-
-            previewLayer.insertSublayer(videoLayer, atIndex: 0)
-
-            _session.startRunning()
-        } catch let error as NSError {
-            NSLog("Error creating session: %@", error.localizedDescription);
+            // Start the preview
+            preview();
         }
 
         // Bring the window up front
@@ -67,16 +51,81 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         return true;
     }
 
+    /* ====================================================================== */
+    /* Device Selection                                                       */
+    /* ====================================================================== */
+
+    func switchDevice(sender:NSMenuItem) {
+        let device:AVCaptureDevice = sender.representedObject as! AVCaptureDevice
+        NSLog("Selecting device: " + device.localizedName)
+        _device = device
+        preview()
+    }
+
+    @IBAction func selectDevice(sender:NSButton) {
+
+        let menu = NSMenu()
+        menu.autoenablesItems = false
+
+        for device in AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo) {
+            let item = menu.addItemWithTitle(device.localizedName, action: #selector(switchDevice), keyEquivalent: "")
+            if device.isEqualTo(_device) {
+                item?.state = NSOnState
+                item?.enabled = false
+            }
+            item?.representedObject = device
+            item?.target = self
+        }
+
+        let event:NSEvent? = NSApplication.sharedApplication().currentEvent
+        NSMenu.popUpContextMenu(menu, withEvent: event!, forView: sender)
+    }
+
+    /* ====================================================================== */
+    /* Format Selection                                                       */
+    /* ====================================================================== */
+
+    func switchFormat(sender:NSMenuItem) {
+        let format:AVCaptureDeviceFormat = sender.representedObject as! AVCaptureDeviceFormat
+        NSLog("Selecting format: " + format.localizedName)
+
+        // Reconfigure the format and rescale the window
+        do {
+            try _device!.lockForConfiguration()
+            _device!.activeFormat = format
+            _device!.unlockForConfiguration()
+            resize()
+        } catch let error as NSError {
+            NSLog("Error configuring: %@", error.localizedDescription);
+        }
+    }
+
+    @IBAction func selectFormat(sender:NSButton) {
+        if _device == nil { return }
+
+        let menu = NSMenu()
+        menu.autoenablesItems = false
+
+        for format in _device!.formats {
+            if (CMFormatDescriptionGetMediaType(format.formatDescription) == kCMMediaType_Video) {
+                let item = menu.addItemWithTitle(format.localizedName, action: #selector(switchFormat), keyEquivalent: "")
+                if format.isEqualTo(_device!.activeFormat) {
+                    item?.state = NSOnState
+                    item?.enabled = false
+                }
+                item?.representedObject = format
+                item?.target = self
+            }
+        }
+
+        let event:NSEvent? = NSApplication.sharedApplication().currentEvent
+        NSMenu.popUpContextMenu(menu, withEvent: event!, forView: sender)
+    }
+    
 
     /* ====================================================================== */
 
-    @IBAction func clickSettings(sender:NSButton) {
-        if settings.visible { return }
-        settings.setFrameTopLeftPoint(NSEvent.mouseLocation())
-        settings.makeKeyAndOrderFront(sender)
-    }
-
-    @IBAction func clickCamera(sender:NSButton) {
+    @IBAction func takePicture(sender:NSButton) {
         if _capture.connections.count < 1 {
             NSLog("No connections to capture");
             return
@@ -114,71 +163,58 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     }
 
     /* ====================================================================== */
+    /* Preview and resizing                                                   */
+    /* ====================================================================== */
 
-    private var _selectedDevice:AVCaptureDevice? = nil;
+    func preview() {
+        // Stop, clear and recreate session
+        _session.stopRunning()
+        _session.removeOutput(_capture)
+        _session = AVCaptureSession()
 
-    var availableDevices:[AVCaptureDevice] {
-        get {
-            let devices:[AnyObject] = AVCaptureDevice.devicesWithMediaType(AVMediaTypeVideo)
-            return devices as! [AVCaptureDevice]
-        }
-    }
+        do {
+            // Create input and add it to the session
+            let input:AVCaptureInput = try AVCaptureDeviceInput(device:_device)
+            _session.addInput(input)
 
-    var selectedDevice:AVCaptureDevice? {
-        get {
-            if (_selectedDevice == nil) {
-                _selectedDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+            // Create output and add it to the session
+            _capture.outputSettings = [ AVVideoCodecKey: AVVideoCodecJPEG ]
+            _session.addOutput(_capture)
+
+            // Remove everything from the preview layer
+            let previewLayer:CALayer = window.contentView!.layer!
+            if (previewLayer.sublayers?.first is AVCaptureVideoPreviewLayer) {
+                NSLog("Replacing preview layer");
+                previewLayer.sublayers?.removeFirst()
+            } else {
+                NSLog("Appending preview layer");
             }
-            return _selectedDevice
+
+            // Add our new preview layer
+            let videoLayer = AVCaptureVideoPreviewLayer(session: _session)
+            videoLayer.frame = previewLayer.bounds
+            videoLayer.autoresizingMask = CAAutoresizingMask.LayerWidthSizable.union(CAAutoresizingMask.LayerHeightSizable)
+            videoLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+            previewLayer.insertSublayer(videoLayer, atIndex: 0)
+
+            // Start the preview session
+            resize()
+            _session.startRunning()
+        } catch let error as NSError {
+            NSLog("Error creating session: %@", error.localizedDescription);
         }
-        set {
-            NSLog("Selected %@", newValue == nil ? "[nil]" : newValue!.localizedName)
-            _selectedDevice = newValue
-            rere()
-        }
+
     }
 
-    var availableFormats:[AVCaptureDeviceFormat] {
-        get {
-            let device:AVCaptureDevice? = self.selectedDevice
-            if device == nil { return [] }
+    private func resize() {
+        if _device == nil { return }
 
-            let formats:[AnyObject]? = device!.formats
-            if formats == nil { return [] }
-
-            return formats as! [AVCaptureDeviceFormat]
-        }
-    }
-
-    var selectedFormat:AVCaptureDeviceFormat? {
-        get {
-            if _selectedDevice == nil { return nil }
-            return _selectedDevice!.activeFormat
-        }
-        set {
-            NSLog("Selected %@", newValue == nil ? "[nil]" : newValue!.localizedName)
-            if (newValue != nil) && (_selectedDevice != nil) {
-                do {
-                    try _selectedDevice!.lockForConfiguration()
-                    _selectedDevice!.activeFormat = newValue
-                    _selectedDevice!.unlockForConfiguration()
-                    rere()
-                } catch let error as NSError {
-                    NSLog("Error configuring: %@", error.localizedDescription);
-                }
-            }
-        }
-    }
-
-    private func rere() {
-        if _selectedDevice == nil { return }
-
-        let description = _selectedDevice!.activeFormat.formatDescription
+        let description = _device!.activeFormat.formatDescription
         let dimensions:CMVideoDimensions = CMVideoFormatDescriptionGetDimensions(description)
 
         let x = window.frame.origin.x
-        let width = CGFloat(dimensions.width)
-        let height = CGFloat(dimensions.height)
+        let width = CGFloat(dimensions.width < 240 ? 240 : dimensions.width)
+        let height = CGFloat(dimensions.height < 160 ? 160 : dimensions.height)
 
         let y = window.frame.origin.y + window.frame.size.height - height
 
